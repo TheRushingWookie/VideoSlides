8c01f942c5facea6aa07c0794c3a99ece6c910ee
Quinn Jarrell <qjarrell@gosynapsify.com>
2015-04-04 23:48:29 -0500

    moved to checking corners between images

--- test.py
+++ test.py
@@ -3,53 +3,79 @@ import numpy as np
 import cv2
 import math
 import sys
+import cv
 cap = cv2.VideoCapture(sys.argv[1])
-
+import sys
 prev = None
 runOnce = False
 count = 0
 prefix = "./img_"
-avg = 0
+avg = 1
 num_frames = 0
 threshold = int(sys.argv[2])
+num_corners = 0
 while True:
     # Capture frame-by-frame
+    for i in range(50):
+        ret = cap.grab()
+        if not ret:
+            sys.exit(0)
     ret, frame = cap.read()
+    
     # Our operations on the frame come here
     if ret:
-        if True:
+        newFrame = cv2.resize(frame, (100,100))
+        if False:
             gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
             edges = cv2.Canny(gray,100,200)
             gray = np.float32(edges)
-            dst = cv2.cornerHarris(gray,2,3,0.04)
+            dst = cv2.cornerHarris(gray,3,5,0.04)
+            dst = cv2.normalize( dst );
+            print dst.sum()
             # Display the resulting frame
             dst = cv2.dilate(dst,None)
 
         # Threshold for an optimal value, it may vary depending on the image.
             edges[dst>0.05*dst.max()]=[255]
             cv2.imshow("edges",edges)
-        if False:
+        if True:
 
             if runOnce:
-                diff = cv2.absdiff(prev, frame).sum() + 1
-                diff = math.log(diff)
+                gray = cv2.cvtColor(newFrame, cv2.COLOR_BGR2GRAY)
+                gray = np.float32(gray)
+                dst = cv2.cornerHarris(gray,2,3,0.04)
+                dst = cv2.normalize( dst );
+                diff = dst.sum()
                 num_frames += 1
                 avg += diff
-                average_val = int((avg + (.5 * avg))/num_frames)
-                if diff > threshold:
+                #diff = cv2.absdiff(prev, frame).sum() + 1
+                #diff = math.log(diff)
+                diff = math.fabs(int(dst.sum()))
+
+                average_val = math.fabs(int((avg + (0 * avg))/num_frames))
+                #print "diff %s avg %s" % (diff, average_val)
+                if math.fabs(average_val - diff) > average_val / 4:
                     cv2.imwrite(prefix + str(count) + ".jpg", frame)
                     count += 1
                     #cv2.imshow('frame',frame)
                     print "diff %s avg %s" % (diff, average_val)
+                    num_frames = 0
+                    
+                    avg = 0
+
+                if num_frames == 30:
+                    num_frames = 0
+                    avg = 0
             else:
                 cv2.imwrite(prefix + str(count) + ".jpg", frame)
+                count+=1
                 runOnce = True
             """for x in range(len(frame)):
                 for y in range(len(frame[x])):
                     frame[x][y] = [0,255,0]"""
         prev = frame
         
-        count += 1
+        
     else:
         print "done"
         break
